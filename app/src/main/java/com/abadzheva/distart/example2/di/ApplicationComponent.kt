c2d98ff7614320cc355f9763c5e826f6c43a7d39
k8abadzheva <make.me.loco@gmail.com>
2024-11-07 03:42:08 +0300

    feat: Inject ViewModels with ViewModelFactory and Dagger
    Introduced `ViewModelFactory` to inject dependencies into ViewModels using a map of ViewModel providers.
    Added `MainActivity2` and injected `ExampleViewModel` and `ExampleViewModel2` using the factory.
    Updated `MainActivity` to inject ViewModels using the factory and added navigation to `MainActivity2`.
    Updated `ApplicationComponent` to inject dependencies into `MainActivity2`.

--- app/src/main/java/com/abadzheva/distart/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/abadzheva/distart/example2/di/ApplicationComponent.kt
@@ -2,6 +2,7 @@ package com.abadzheva.distart.example2.di
 
 import android.content.Context
 import com.abadzheva.distart.example2.presentation.MainActivity
+import com.abadzheva.distart.example2.presentation.MainActivity2
 import dagger.BindsInstance
 import dagger.Component
 
@@ -9,6 +10,8 @@ import dagger.Component
 @Component(modules = [DataModule::class, DomainModule::class, ViewModelModule::class])
 interface ApplicationComponent {
     fun inject(activity: MainActivity)
+
+    fun inject(activity: MainActivity2)
 
     @Component.Factory
     interface ApplicationComponentFactory {
